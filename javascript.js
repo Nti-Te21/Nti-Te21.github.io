9c30f6deb893d31c1a86e66878bd15ff84f84c52
Nti-Te21 <alec.andin@elev.ga.ntig.se>
2024-03-25 09:16:48 +0100

    progeesrasasdaf ss

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function init() {
     const nav = document.createElement('nav');
-    nav.innerHTML = '<a href="#">Startsdia</a> | <a href="#">Kurser</a> | <a href="#">prisinformation</a> <a href="#">exempel</a><a href="#">recensioner</a> ';
+    nav.innerHTML = '<a href="index.html">Startsida</a> | <a href="kurser.html">Kurser</a> | <a href="priser.html">Priser</a> | <a href="exempel.html">Exempel</a> | <a href="recensioner.html">Recensioner</a> ';
     const footer = document.createElement('footer');
     footer.innerHTML = '<p>&copy; 2024 MyWebsite. All rights reserved.</p>';
     document.body.appendChild(nav);
