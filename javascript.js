0fd529d90f51f042c2e3f7316d6d3abb0f8d34c6
Nti-Te21 <alec.andin@elev.ga.ntig.se>
2024-03-25 09:09:16 +0100

    small change

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function init() {
     const nav = document.createElement('nav');
-    nav.innerHTML = '<a href="#">Home</a> | <a href="#">About</a> | <a href="#">Contact</a>';
+    nav.innerHTML = '<a href="#">Startsdia</a> | <a href="#">Kurser</a> | <a href="#">prisinformation</a> <a href="#">exempel</a><a href="#">recensioner</a> ';
     const footer = document.createElement('footer');
     footer.innerHTML = '<p>&copy; 2024 MyWebsite. All rights reserved.</p>';
     document.body.appendChild(nav);
