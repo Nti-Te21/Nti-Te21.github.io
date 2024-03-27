e5f056aeec251da8196a007a41f2d3f88fbf4821
Nti-Te21 <alec.andin@elev.ga.ntig.se>
2024-03-27 14:20:11 +0100

    Progress all text very good looking

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 function init() {
-    const nav = document.createElement('nav');
+    const nav = document.querySelector('nav');
     nav.innerHTML = '<a href="index.html">Startsida</a> | <a href="kurser.html">Kurser</a> | <a href="priser.html">Priser</a> | <a href="exempel.html">Exempel</a> | <a href="recensioner.html">Recensioner</a> ';
-    const footer = document.createElement('footer');
+    const footer = document.querySelector('footer');
     footer.innerHTML = '<p>&copy; 2024 MyWebsite. All rights reserved.</p>';
-    document.body.appendChild(nav);
-    document.body.appendChild(footer);
 }
 init();
